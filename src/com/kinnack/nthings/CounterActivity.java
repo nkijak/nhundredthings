c8c467d41cd272f32c223b16f525036812020bd2
nkijak <nkijak@cabb95f2-ea03-11de-a70e-4b51a9783938>
2010-06-11 04:57:29 +0000

    Fixed return count value from counter activity

--- src/com/kinnack/nthings/CounterActivity.java
+++ src/com/kinnack/nthings/CounterActivity.java
@@ -154,7 +154,7 @@ public class CounterActivity extends Activity {
     
     private Intent createIntentWithStats(int reps_) {
         Intent intent = new Intent();
-        intent.putExtra(MAX_COUNT, neededCount);
+        intent.putExtra(MAX_COUNT, reps_);
         intent.putExtra(AVG_TIME, sumTimeBetweenCounts/reps_);
         intent.putExtra(TOTAL_TIME, sumTimeBetweenCounts);
         return intent;
