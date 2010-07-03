e6050cbdc73f205295c6337daf9ce7c2addd56b5
nkijak <nkijak@cabb95f2-ea03-11de-a70e-4b51a9783938>
2010-07-03 15:59:43 +0000

    Added ranged validation to manual entry field

--- src/com/kinnack/nthings/activity/ManualEntryCounterActivity.java
+++ src/com/kinnack/nthings/activity/ManualEntryCounterActivity.java
@@ -2,16 +2,48 @@ package com.kinnack.nthings.activity;
 
 import com.kinnack.nthings.R;
 import com.kinnack.nthings.StopWatch;
+import com.kinnack.nthings.helper.RangedIntTextWatcher;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.EditText;
 
 public class ManualEntryCounterActivity extends CounterActivity {
+    private RangedIntTextWatcher rangedIntTextWatcher;
     @Override
     public void onCreate(Bundle savedInstanceState_) {
         super.onCreate(savedInstanceState_);
         stopWatch = new StopWatch();
+        rangedIntTextWatcher = new RangedIntTextWatcher(0, 300);
+        EditText manualEntry = (EditText)findViewById(R.id.manualEntry);
+        manualEntry.addTextChangedListener(rangedIntTextWatcher);
+        // XXX Hack that depends on TextChangedListeners to be called in the order they are added!!
+        manualEntry.addTextChangedListener(new TextWatcher() {
+            
+            @Override
+            public void onTextChanged(CharSequence s_, int start_, int before_, int count_) {
+                View endButton = findViewById(R.id.Done);
+                if (rangedIntTextWatcher.isValid()) {
+                    endButton.setEnabled(false);                    
+                } else {
+                    endButton.setEnabled(true);  
+                }
+            }
+            
+            @Override
+            public void beforeTextChanged(CharSequence s_, int start_, int count_, int after_) {
+                // TODO Auto-generated method stub
+                
+            }
+            
+            @Override
+            public void afterTextChanged(Editable s_) {
+                // TODO Auto-generated method stub
+                
+            }
+        });
     }
     
     public void startTimer(View target_) {
@@ -23,8 +55,8 @@ public class ManualEntryCounterActivity extends CounterActivity {
     
     public void endTimer(View target_) {
         stopWatch.stop();
+        // DONE button only enabled after validation
         findViewById(R.id.endBtn).setEnabled(false);
-        findViewById(R.id.Done).setEnabled(true);
         findViewById(R.id.manualEntry).setEnabled(true);
         sumTimeBetweenCounts = stopWatch.getElapsedTime();
         if (increment == -1) {
---
+++ src/com/kinnack/nthings/helper/RangedIntTextWatcher.java
@@ -0,0 +1,56 @@
+package com.kinnack.nthings.helper;
+
+import android.content.Context;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.Toast;
+
+public class RangedIntTextWatcher implements TextWatcher{
+    private int start;
+    private int end;
+    private boolean isValid;
+    
+    public RangedIntTextWatcher(int start_, int end_) {
+        start = start_;
+        end = end_;
+    }
+    
+    @Override
+    public void afterTextChanged(Editable s_) {
+        int value = start;
+        isValid = false;
+        try {
+            value = Integer.parseInt(s_.toString());
+            if (value < start) {
+                value = 0;
+            } else if(value > end) {
+                value = end;
+            }
+            
+        } catch(NumberFormatException nfe) {
+        }
+        if (!String.valueOf(value).equals(s_.toString())) {
+            s_.replace(0, s_.length(), value+"", 0, (value+"").length());
+        }
+        isValid = true;
+        
+    }
+
+    @Override
+    public void beforeTextChanged(CharSequence s_, int start_, int count_, int after_) {
+        // don't care
+    }
+
+    @Override
+    public void onTextChanged(CharSequence s_, int start_, int before_, int count_) {
+        // dont' care
+    }
+
+    /**
+     * @return the isValid
+     */
+    public boolean isValid() {
+        return isValid;
+    }
+
+}
