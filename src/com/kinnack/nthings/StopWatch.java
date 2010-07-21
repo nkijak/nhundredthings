da7a9a741914d6bb9b463ac015e9d7da6cc7289d
davelnewton <davelnewton@cabb95f2-ea03-11de-a70e-4b51a9783938>
2010-07-21 10:02:52 +0000

    Testing commit rights.

--- src/com/kinnack/nthings/StopWatch.java
+++ src/com/kinnack/nthings/StopWatch.java
@@ -16,7 +16,6 @@ public class StopWatch {
     private long startTime = 0;
     private long stopTime = 0;
     private boolean running = false;
-
     
     public void start() {
         this.startTime = System.currentTimeMillis();
