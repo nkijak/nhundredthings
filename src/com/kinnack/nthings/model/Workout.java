c8dbb2f7b3927756a832cdbeaaf31c0e1ced1d52
nkijak <nkijak@cabb95f2-ea03-11de-a70e-4b51a9783938>
2010-12-30 18:48:47 +0000

    added situp information

--- src/com/kinnack/nthings/model/Workout.java
+++ src/com/kinnack/nthings/model/Workout.java
@@ -313,12 +313,328 @@ public class Workout {
             }
         }
     };
-        
+    
+  //[week][day][level][counts][rests]
+    protected static final int[][][][][] SITUPS = new int[][][][][] {
+        // week 1
+        {
+            // day 1 
+            {
+                
+                // easy
+                {
+                    {3,4,3,3,5},STANDARD_REST
+                },
+                // med
+                {
+                    {9,9,6,6,8},STANDARD_REST
+                },
+                // hard
+                {
+                    {15,18,10,10,14},STANDARD_REST
+                }
+            },
+         // day 2 
+            {
+                
+                // easy
+                {
+                    {6,6,3,5,6}, STANDARD_REST
+                },
+                // med
+                {
+                    {9,12,9,9,10}, STANDARD_REST
+                },
+                // hard
+                {
+                    {15,18,15,15,18}, STANDARD_REST
+                }
+            },
+         // day 3 
+            {
+                
+                // easy
+                {
+                    {6,7,6,6,8}, STANDARD_REST
+                },
+                // med
+                {
+                    {12,15,11,11,15}, STANDARD_REST
+                },
+                // hard
+                {
+                    {17,22,14,14,20}, STANDARD_REST
+                }
+            }
+        },
+     // week 2
+        {
+            // day 1 
+            {
+                
+                // easy
+                {
+                    {6,9,6,6,9},STANDARD_REST
+                },
+                // med
+                {
+                    {14,17,12,12,17},STANDARD_REST
+                },
+                // hard
+                {
+                    {21,21,15,15,22},STANDARD_REST
+                }
+            },
+         // day 2 
+            {
+                
+                // easy
+                {
+                    {7,9,6,6,11},STANDARD_REST
+                },
+                // med
+                {
+                    {15,18,14,14,20}, STANDARD_REST
+                },
+                // hard
+                {
+                    {21,24,18,18,26}, STANDARD_REST
+                }
+            },
+         // day 3 
+            {
+                
+                // easy
+                {
+                    {8,12,8,8,12}, STANDARD_REST
+                },
+                // med
+                {
+                    {18,20,15,15,23}, STANDARD_REST
+                },
+                // hard
+                {
+                    {24,25,21,21,30}, STANDARD_REST
+                }
+            }
+        },
+     // week 3
+        {
+            // day 1 
+            {
+                
+                // easy
+                {
+                    {15,18,11,11,14}, STANDARD_REST
+                },
+                // med
+                {
+                    {18,25,19,19,25}, STANDARD_REST
+                },
+                // hard
+                {
+                    {21,27,21,21,30}, STANDARD_REST
+                }
+            },
+         // day 2 
+            {
+                
+                // easy
+                {
+                    {15,18,12,12,18}, STANDARD_REST
+                },
+                // med
+                {
+                    {21,18,21,21,28}, STANDARD_REST
+                },
+                // hard
+                {
+                    {30,38,23,23,38}, STANDARD_REST
+                }
+            },
+         // day 3 
+            {
+                
+                // easy
+                {
+                    {17,20,14,14,20}, STANDARD_REST
+                },
+                // med
+                {
+                    {24,32,23,23,32}, STANDARD_REST
+                },
+                // hard
+                {
+                    {33,42,30,30,45}, STANDARD_REST
+                }
+            }
+        },
+     // week 4
+        {
+            // day 1 
+            {
+                
+                // easy
+                {
+                    {18,21,17,15,24}, STANDARD_REST
+                },
+                // med
+                {
+                    {27,33,24,24,38}, STANDARD_REST
+                },
+                // hard
+                {
+                    {32,38,32,32,48}, STANDARD_REST
+                }
+            },
+         // day 2 
+            {
+                
+                // easy
+                {
+                    {21,24,18,18,27}, STANDARD_REST
+                },
+                // med
+                {
+                    {30,38,30,30,42}, STANDARD_REST
+                },
+                // hard
+                {
+                    {38,45,38,38,54}, STANDARD_REST
+                }
+            },
+         // day 3 
+            {
+                
+                // easy
+                {
+                    {24,27,20,20,30}, STANDARD_REST
+                },
+                // med
+                {
+                    {35,42,35,35,50}, STANDARD_REST
+                },
+                // hard
+                {
+                    {45,50,45,45,60}, STANDARD_REST
+                }
+            }
+        },
+     // week 5
+        {
+            // day 1 
+            {
+                
+                // easy
+                {
+                    {26,30,23,23,30}, STANDARD_REST
+                },
+                // med
+                {
+                    {42,52,38,33,52}, STANDARD_REST
+                },
+                // hard
+                {
+                    {54,60,45,36,60}, STANDARD_REST
+                }
+            },
+         // day 2 
+            {
+                
+                // easy
+                {
+                    {15,15,20,20,15,15,15,38},SHORT_REST
+                },
+                // med
+                {
+                    {27,27,30,30,21,21,24,60}, SHORT_REST
+                },
+                // hard
+                {
+                    {30,30,36,36,27,27,33}, SHORT_REST
+                }
+            },
+         // day 3 
+            {
+                
+                // easy
+                {
+                    {18,18,22,22,18,18,15,45}, SHORT_REST
+                },
+                // med
+                {
+                    {26,26,30,30,26,26,30,67}, SHORT_REST
+                },
+                // hard
+                {
+                    {30,30,36,36,30,30,40,75}, SHORT_REST
+                }
+            }
+        },
+     // week 6
+        {
+            // day 1 
+            {
+                
+                // easy
+                {
+                    {38,45,30,22,60}, STANDARD_REST
+                },
+                // med
+                {
+                    {60,75,38,35,75}, STANDARD_REST
+                },
+                // hard
+                {
+                    {70,85,52,45,85}, STANDARD_REST
+                }
+            },
+         // day 2 
+            {
+                
+                // easy
+                {
+                    {21,21,23,23,21,21,15,15,66}, SHORT_REST
+                },
+                // med
+                {
+                    {30,30,35,35,30,30,27,27,80}, SHORT_REST
+                },
+                // hard
+                {
+                    {33,33,45,45,36,36,32,32,90}, SHORT_REST
+                }
+            },
+         // day 3 
+            {
+                
+                // easy
+                {
+                    {20,20,26,26,24,24,21,21,75}, SHORT_REST
+                },
+                // med
+                {
+                    {33,33,45,45,34,34,27,27,90}, SHORT_REST
+                },
+                // hard
+                {
+                    {39,39,50,50,39,39,33,33,105}, SHORT_REST
+                }
+            }
+        }
+    };
         
     public static ExerciseSet getPushupSetFor(int week, int day, Level level) {
         // TODO error handling, argument checking
         Log.d("dgmt:Workout.getPushupSetFor", String.format("Getting pushups for week %d, day %d, level %s",week,day,level));
         int[][] countRest = PUSHUPS[week-1][day-1][level.getIndex()];
+        return new ExerciseSet(countRest[0], countRest[1]);
+    }
+    
+    public static ExerciseSet getSitupSetFor(int week, int day, Level level) {
+     // TODO error handling, argument checking
+        Log.d("dgmt:Workout.getSitupSetFor", String.format("Getting situps for week %d, day %d, level %s",week,day,level));
+        int[][] countRest = SITUPS[week-1][day-1][level.getIndex()];
         return new ExerciseSet(countRest[0], countRest[1]);
     }
         
